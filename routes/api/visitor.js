956fbc93bec5fa1fb4f786f187f35214b404eb77
rStarboy <rStarboy0809@gmail.com>
2022-02-04 20:05:23 +0300

    update

--- routes/api/visitor.js
+++ routes/api/visitor.js
@@ -15,7 +15,6 @@ const { lookup } = require('geoip-lite');
 // @desc     Register user
 // @access   Public
 router.post('/', async (req, res) => {
-    console.log('-----------------------------------req')
     const detector = new DeviceDetector;
 
     const device = detector.detect(req.headers['user-agent']).device.model;
