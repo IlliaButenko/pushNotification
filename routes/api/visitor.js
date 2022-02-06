8671a22e7f9a0772ef5c90fcfec0f8a96250b8d7
rStarboy <rStarboy0809@gmail.com>
2022-02-06 18:15:13 +0300

    Update visitor.js

--- routes/api/visitor.js
+++ routes/api/visitor.js
@@ -20,7 +20,7 @@ router.post('/', async (req, res) => {
 
     const device = DVC.device.model;
     const user_ip = req.headers['x-real-ip'] || req.connection.remoteAddress;
-    console.log(user_ip, lookup(user_ip).country);
+    console.log(user_ip);
     const country = lookup(user_ip).country;
     // const country = 'US'
     let system = req.headers['sec-ch-ua-platform'];
