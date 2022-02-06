320ba0df2383d0b880e10fe7e93c46baaa6882fd
rStarboy <rStarboy0809@gmail.com>
2022-02-06 16:56:48 +0300

    Update notification.js

--- routes/api/notification.js
+++ routes/api/notification.js
@@ -22,20 +22,21 @@ router.post('/send', async (req, res) => {
         for (let i = 0; i < sysValue.length; i++) {
 
             const visitor = await Visitor.find({ system: { $regex: '.*' + sysValue[i].title + '.*' } })
-            if (methodValue === 'Individual' && visitor.length > 0) {
+            const visitorLength = visitor.length;
+            if (methodValue === 'Individual' && visitorLength > 0 && visitorLength < userNo) {
                 const subscription = JSON.parse(visitor[parseInt(userNo) - 1].subscription);
                 console.log(visitor[parseInt(userNo) - 1], subscription)
                 sendNotification(subscription, payload);
-            } else if (methodValue === 'Partial' && visitor.length > 0) {
+            } else if (methodValue === 'Partial' && visitorLength > 0) {
 
                 const iA = userRange.split('-')
                 const from = parseInt(iA[0]) - 1;
                 const to = parseInt(iA[1]) - 1
 
-                if (from > visitor.length || from < 0) {
+                if (from > visitorLength || from < 0) {
                     continue;
                 }
-                for (let j = from; j < visitor.length; j++) {
+                for (let j = from; j < visitorLength; j++) {
                     const subscription = JSON.parse(visitor[j].subscription);
                     sendNotification(subscription, payload);
                     if (j === to) {
