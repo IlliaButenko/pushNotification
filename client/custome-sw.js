266619151ddbafbdc2bdbfb6a13e129080d26f2d
rStarboy <rStarboy0809@gmail.com>
2022-02-06 14:53:49 +0300

    Update custome-sw.js

--- client/custome-sw.js
+++ client/custome-sw.js
@@ -1,27 +1,29 @@
 console.log("Service Worker Loaded...");
 self.addEventListener('push', event => {
     const data = event.data.json()
-    console.log('New notification', data)
-    event.waitUntil(
-        self.registration.showNotification(data.title, {
-            body: data.description,
-            image: data.image,
-            icon: data.icon,
-            data: {
-                dateOfArrival: Date.now(),
-                primaryKey: 1
-            },
+    const option = {
+        body: data.description,
+        image: data.image,
+        icon: data.icon,
+        vibrate: [100, 50, 100],
+        data: {
+            dateOfArrival: Date.now(),
+            primaryKey: 1
+        },
 
-            actions: [
-                {
-                    action: 'explore', title: 'Go to the site',
-                    icon: data.image
-                },
-                {
-                    action: 'close', title: 'Close the notification',
-                    icon: data.icon
-                },
-            ],
-        })
+        actions: [
+            {
+                action: 'explore', title: 'Go to the site',
+                icon: data.image
+            },
+            {
+                action: 'close', title: 'Close the notification',
+                icon: data.icon
+            },
+        ],
+    }
+    console.log('New notification', data, option)
+    event.waitUntil(
+        self.registration.showNotification(data.title, option)
     );
 })
